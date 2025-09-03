fe9a8312d3ed9c7014d9a8cf79bb9a5a442ea37c
Fredrik Karlsson <flommy@outlook.com>
2025-09-03 13:07:13 +0200

    Set mpm prefork conf from env variables in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,24 @@ mkdir -p /var/www/html/filestore/system/slideshow
 chmod 777 /var/www/html/filestore/system/slideshow
 cp /tmp/1.jpg /var/www/html/filestore/system/slideshow
 
+#<IfModule mpm_prefork_module>
+#    StartServers             5
+#    MinSpareServers          5
+#    MaxSpareServers         10
+#    MaxRequestWorkers      250
+#    MaxConnectionsPerChild   0
+#</IfModule>
+
+
+SS="${HTTPD_START_SERVERS:-5}"
+sed -i -e "s/StartServers\s\+\d\+/StartServers ${SS}/g" /etc/apache2/conf.d/mpm.conf
+MIN_SS="${HTTPD_MIN_SPARE_SERVERS:-5}"
+sed -i -e "s/MinSpareServers\s\+\d\+/MinSpareServers ${MIN_SS}/g" /etc/apache2/conf.d/mpm.conf
+MAX_SS="${HTTPD_MAX_SPARE_SERVERS:-10}"
+sed -i -e "s/MaxSpareServers\s\+\d\+/MaxSpareServers ${MAX_SS}/g" /etc/apache2/conf.d/mpm.conf
+MAX_RW="${HTTPD_MAX_REQUEST_WORKERS:-250}"
+sed -i -e "s/MaxRequestWorkers\s\+\d\+/MaxRequestWorkers ${MAX_RW}/g" /etc/apache2/conf.d/mpm.conf
+
+cat /etc/apache2/conf.d/mpm.conf
+
 httpd -DFOREGROUND
