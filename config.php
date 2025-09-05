f994243a25441007c6725d5d771624b1155ec57c
Fredrik Karlsson <flommy@outlook.com>
2025-09-05 16:02:20 +0200

    Added support for UPLOAD_THEN_PROCESS mode

--- config.php
+++ config.php
@@ -80,7 +80,19 @@ $ffmpeg_preview_extension = 'mp4';
 $ffmpeg_preview_options = '-f mp4 -b:v 1200k -b:a 64k -ac 1 -c:v libx264 -pix_fmt yuv420p -profile:v baseline -level 3 -c:a aac -strict -2';
 
 $daterange_search = true;
-$upload_then_edit = true;
+if (getenv('UPLOAD_THEN_PROCESS')) {
+    // New upload mode that focuses on getting files into the filestore, then working off a queue for further processing (metadata extract, preview creation, etc).
+    // requires $offline_job_queue=true;
+    $upload_then_process = true;
+
+    // Uncomment and set to an archive state where $upload_then_process files are stored before processing.
+    // It is strongly recommended that a unique archive state be created to handle this
+    $upload_then_process_holding_state = -3;
+    $lang['status-3'] = "Pending upload processing";
+} else {
+    // New mode that means the upload goes first, then the users edit and approve resources moving them to the correct stage.
+    $upload_then_edit = true;
+}
 
 $purge_temp_folder_age=90;
 $filestore_evenspread=true;
